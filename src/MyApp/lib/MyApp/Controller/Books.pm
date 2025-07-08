1f6d8d1a16781f23c08efd2cd55160e74cd22a80
Ari Romero Garrido <ari.romero@gmail.com>
2025-07-08 18:00:41 -0400

    add Books controller

---
+++ src/MyApp/lib/MyApp/Controller/Books.pm
@@ -0,0 +1,55 @@
+package MyApp::Controller::Books;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+MyApp::Controller::Books - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Books Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+    $c->response->body('Matched MyApp::Controller::Books in Books.');
+}
+
+=head2 index
+
+=cut
+
+sub list :Local :Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash(books => '');
+    $c->stash(template => 'books/list.tt2')
+}
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+root
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
